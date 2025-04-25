e64b8b4b2582396cbbd20363ca74dc5545362755
Chinaza11 <70081694+Chinaza11@users.noreply.github.com>
2025-04-24 21:33:12 -0400

    Update sv_stats.R
    violin plot of indels length per chromosome

--- main/sv_stats.R
+++ main/sv_stats.R
@@ -3,6 +3,7 @@ ls()
 
 library(tidyverse)
 library(gtools)
+library(ggpubr)
 
 setwd("G:/My Drive/PhD/project/structural_variant_genotyping_with_pangenome_graph/data_analysis/data_and_result/")
 
@@ -38,25 +39,18 @@ dev.off()
 
 
 # =============================================================================
-# Violin plot of indels per chromosome
+# Violin plot of indels length per chromosome
 # =============================================================================
 
 df = read.table("main/sv/sv_stats_indels_len_per_chrom.txt", header=TRUE, sep="")
 
 library(ggplot2)
 
-set.seed(42)
-df <- data.frame(
-  Chromosome = rep(c("chr1", "chr2", "chr3"), each = 20),
-  Position = sample(100:10000, 60, replace = TRUE),
-  Type_of_Variant = rep(c("Insertion", "Deletion"), 30),
-  Length = c(
-    sample(1:10, 30, replace = TRUE),  # Insertions
-    sample(3:15, 30, replace = TRUE)   # Deletions
-  )
-)
+df$Chromosome <- factor(df$Chromosome,
+                             levels = paste0("chr", 1:10))
 
-# Violin plot
+png(file="main/sv/sv_stats_indels_len_per_chrom.png", width=14, height=8, units="in", res=500)
+
 ggplot(df, aes(x = Chromosome, y = log10(Length), fill = Type_of_Variant)) +
   geom_violin(position = position_dodge(width = 0.9), trim = FALSE, alpha = 0.7) +
   geom_boxplot(position = position_dodge(width = 0.9), width = 0.2, outlier.shape = NA) +
@@ -64,3 +58,42 @@ ggplot(df, aes(x = Chromosome, y = log10(Length), fill = Type_of_Variant)) +
        x = "Chromosome", y = "Length") +
   scale_fill_manual(values = c("Insertion" = "#1f77b4", "Deletion" = "#ff7f0e")) +
   theme_bw()
+
+dev.off()
+
+# =============================================================================
+
+df_del <- subset(df, Type_of_Variant == "Deletion")
+df_ins <- subset(df, Type_of_Variant == "Insertion")
+
+del = ggplot(df_del, aes(x=Chromosome, y=log10(Length), fill=Type_of_Variant)) +
+        geom_violin(trim=F) +
+        geom_boxplot(width = 0.15, outlier.shape = NA, fill = "white") +
+        labs(title = "Distribution of Deletion Lengths by Chromosome",
+             x = "Chromosome", y = "log10(Length)") +
+        scale_fill_manual(values = c("Deletion" = "#ff7f0e")) +
+        theme_bw() +
+        theme(legend.position = "none",
+              axis.title.x = element_blank(),
+              axis.text.x = element_blank(),
+              axis.ticks.x = element_blank()) +
+        scale_y_continuous(limits = c(0, 7), breaks = seq(0, 7, by = 1))
+
+ins = ggplot(df_ins, aes(x=Chromosome, y=log10(Length), fill=Type_of_Variant)) +
+        geom_violin(trim=F) +
+        geom_boxplot(width = 0.15, outlier.shape = NA, fill = "white") +
+        labs(title = "Distribution of Insertion Lengths by Chromosome",
+             x = "Chromosome", y = "log10(Length)") +
+        scale_fill_manual(values = c("Insertion" = "#1f77b4")) +
+        theme_bw() +
+        theme(legend.position = "none") +
+        scale_y_continuous(limits = c(0, 7), breaks = seq(0, 7, by = 1))
+
+
+png(file="main/sv/sv_stats_indels_len_per_chrom_2.png", width=14, height=8, units="in", res=500)
+
+ggarrange(del, ins, 
+          nrow=2,
+          labels = "AUTO")
+
+dev.off()
