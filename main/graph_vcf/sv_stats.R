9348463c2a92c0cb03451b27f514270c7741c657
Chinaza11 <70081694+Chinaza11@users.noreply.github.com>
2025-06-01 19:24:48 -0400

    bar plots and violin plots

--- main/graph_vcf/sv_stats.R
+++ main/graph_vcf/sv_stats.R
@@ -25,7 +25,7 @@ df_long <- df_long[mixedorder(df_long$Chromosome), ]
 df_long$Chromosome <- factor(df_long$Chromosome,
                              levels = paste0("chr", 1:10))
 
-png(file="main/sv/sv_stats_indels_per_chrom.png", width=14, height=8, units="in", res=500)
+png(file="main/graph_vcf/sv/sv_stats_indels_per_chrom.png", width=14, height=8, units="in", res=500)
 
 ggplot(df_long, aes(x = Chromosome, y = Count, fill = VariantType)) +
   geom_bar(stat = "identity", position = "dodge") +
@@ -42,7 +42,7 @@ dev.off()
 # Violin plot of indels length per chromosome
 # =============================================================================
 
-df = read.table("main/sv/sv_stats_indels_len_per_chrom.txt", header=TRUE, sep="")
+df = read.table("main/graph_vcf/sv/sv_stats_indels_len_per_chrom.txt", header=TRUE, sep="")
 
 library(ggplot2)
 
