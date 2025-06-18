8e6a447395d74eb8a3631842300236400d744feb
Chinaza11 <70081694+Chinaza11@users.noreply.github.com>
2025-06-18 07:01:20 -0400

    vg_call in parallel

--- main/graph_minigraph-cactus/00_master.sh
+++ main/graph_minigraph-cactus/00_master.sh
@@ -4,7 +4,7 @@
 # run map_reads2 function in 01_graph_minigraph-cactus_yeast.slurm on Wild_Sorghum_WGS files
 #=================================================================================
 
-: << COMMENT
+: << "COMMENT"
 
 for sample in pi302118_1 pi302267_1 pi329250_1 pi329251_1 pi329252_1 pi369484_1 pi369487_1 pi524718_1 pi532564_1 pi532565_1 pi532566_1 pi532568_1 pi535995_1;
 do
@@ -20,7 +20,7 @@ COMMENT
 # run map_reads2 function in 01_graph_minigraph-cactus_yeast.slurm on TERRA_RAW files
 #=================================================================================
 
-#: << COMMENT
+: << "COMMENT"
 
 for file in /projects/cooper_research1/TERRA_RAW/*_R1.fastq.gz;
 do
@@ -31,7 +31,33 @@ do
         rm $sample.01_graph_minigraph-cactus.slurm
 done
 
+COMMENT
+
+#=================================================================================
+# run vg_call function in 01_graph_minigraph-cactus_yeast.slurm on TERRA_RAW files
+#=================================================================================
+
+#: << "COMMENT"
+
+for file in mappings_terra_raw/*.mapped.gam;
+do
+	echo $file
+	sample=$(basename "$file" | awk -F'.' '{print $1}')
+	echo $sample
+        sed "s#placeholder#$sample#g" 01_graph_minigraph-cactus.slurm > $sample.01_graph_minigraph-cactus.slurm
+        sbatch $sample.01_graph_minigraph-cactus.slurm
+        rm $sample.01_graph_minigraph-cactus.slurm
+done
+
 #COMMENT
+
+#=================================================================================
+#
+#=================================================================================
+
+#=================================================================================
+#
+#=================================================================================
 
 #=================================================================================
 #
